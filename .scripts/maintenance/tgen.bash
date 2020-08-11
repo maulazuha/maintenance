be6f00a39e6c11d0d67c73eed8e830a0323ca136
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-08-11 17:06:25 -0400

    commit 505928906 on 20200811

--- .scripts/maintenance/tgen.bash
+++ .scripts/maintenance/tgen.bash
@@ -55,6 +55,6 @@ cd ..
 rm -f gen/*
 sha512sum setupTermuxArch.tar.gz > setupTermuxArch.sha512
 sha512sum -c setupTermuxArch.sha512
-.scripts/maintenance/do.sums.bash
+.scripts/maintenance/do.sums.bash "$@"
 git show
 # tgen.bash EOF
